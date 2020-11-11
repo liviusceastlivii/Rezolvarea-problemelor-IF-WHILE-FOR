bf7e8aa4b43b71b7cb9e4fd64f1e7678265d9e40
liviusceastlivii <70839080+liviusceastlivii@users.noreply.github.com>
2020-11-11 12:26:42 +0300

    Add files via upload

---
+++ problema1_11.11.2020.py
@@ -0,0 +1,11 @@
+n=eval(input("Dati numarul dorit="))
+if n==28:
+    print("28 de zile are luna: Februarie")
+elif n==29:
+    print("29 de zile are luna: Februarie")
+elif n==30:
+    print("30 de zile au lunile: Aprilie, Iunie, Septembrie, Noiembrie")
+elif n==31:
+    print("31 de zile au lunile: Ianuarie, Martie, Mai, Iulie, August, Octombrie, Decembrie")
+else:
+    print("Acest numar de zile nu corespunde la nici o luna")
